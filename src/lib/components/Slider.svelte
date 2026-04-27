4c7303f704d2b5bb3fb02ea2546a02504c69a61d
gerrr-swe <gerardo.ramirez.swe@gmail.com>
2026-04-27 16:14:33 -0600

    feat: add slider component

---
+++ src/lib/components/Slider.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  type Props = {
+    value: number;
+  }
+
+  let  { value = $bindable() } : Props = $props();
+</script>
+
+<input type="range" class="generator-input" min="20" max="218" bind:value />
+
+<style>
+	/* Target Input */
+.generator-input {
+  -webkit-appearance: none;
+  width: 100%;
+  background: transparent;
+  cursor: pointer;
+}
+
+/* --- Webkit Browsers --- */
+.generator-input::-webkit-slider-runnable-track {
+  height: 4px;
+  background: #909090;
+  border-radius: 2px;
+  border: 0px solid #cbd5e1;
+  box-shadow: 0px 2px 0px rgba(0, 0, 0, 0.1);
+}
+
+.generator-input::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  height: 26px;
+  width: 26px;
+  background: #f03861;
+  border-radius: 30px;
+  border: 0px solid #464646;
+  box-shadow: 0px 4px 0px rgba(240, 56, 97, 0);
+  margin-top: -11px;
+}
+
+/* --- Firefox --- */
+.generator-input::-moz-range-track {
+  height: 4px;
+  background: #909090;
+  border-radius: 2px;
+  border: 0px solid #cbd5e1;
+  box-shadow: 0px 2px 0px rgba(0, 0, 0, 0.1);
+}
+
+.generator-input::-moz-range-thumb {
+  height: 26px;
+  width: 26px;
+  background: #f03861;
+  border-radius: 30px;
+  border: 0px solid #464646;
+  box-shadow: 0px 4px 0px rgba(240, 56, 97, 0);
+}
+</style>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,11 @@
 // place files you want to import through the `$lib` alias in this folder.
 import Header from './components/Header.svelte';
 import Button from './components/Button.svelte';
+import Slider from './components/Slider.svelte';
 
-export { Header, Button };
+export { 
+  Header,
+  Button,
+  Slider,
+};
 
